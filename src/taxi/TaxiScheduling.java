5fe82dfba5e1c6e250ba865d90e70896dbfaa4c6
gwvdh <gwvdh97@gmail.com>
2016-11-23 15:48:01 +0100

    commit

--- src/taxi/TaxiScheduling.java
+++ src/taxi/TaxiScheduling.java
@@ -209,7 +209,7 @@ public class TaxiScheduling {
             }
         }
     }
-    /**d
+    /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
