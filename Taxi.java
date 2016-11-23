867defd77a293957651afddaa5aa3c11670621a8
Frank de Jong <frank.049@hotmail.com>
2016-11-23 13:32:48 +0100

    I made a comment

--- Taxi.java
+++ Taxi.java
@@ -3,12 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-//package taxi;
+package taxi;
 
 import java.util.*;
-
-
-
 
 public class Taxi {
     // I made a comment
